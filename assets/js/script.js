d2e40c1773a1d30313175d48ccf2e9bd18123ba4
Waffle <88282807+WaffleKone@users.noreply.github.com>
2022-02-16 20:16:59 -0500

    made buttons and made them responsive to charts already made

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,52 @@ document.addEventListener('DOMContentLoaded', function () {
         // TODO: Update the scoreboard (same logic as in flip button click handler)
 
 })
+
+let totalFlips = 0
+let timesHeads = 0
+let timesTails = 0
+
+let percHeads = 0
+let percTails = 0
+
+let coinFlipNum
+
+const coinFlipAudio = new Audio('assets/sounds/coin-flip.mp3')
+coinFlipAudio.volume = 0.2
+
+document.getElementById("flip").addEventListener("click", function(){
+    coinFlipNum = Math.round(Math.random())
+    if (coinFlipNum == 0 ) { // We use 0 as tails in this case
+        coinFlipAudio.play()
+        timesTails += 1
+        totalFlips += 1
+        document.getElementById("penny-img").src = "assets/images/penny-tails.jpg"
+        document.getElementById("tails").textContent = (timesTails)
+        document.getElementById("responder").textContent = "You rolled tails!"
+        percHeads = (Math.round(timesHeads / totalFlips * 100))
+        percTails = (Math.round(timesTails / totalFlips * 100))
+        document.getElementById("heads-percent").textContent = (`${percHeads}%`)
+        document.getElementById("tails-percent").textContent = (`${percTails}%`)
+    }
+    if (coinFlipNum == 1) { // We use 1 as heads in this case
+        coinFlipAudio.play()
+        timesHeads += 1
+        totalFlips += 1
+        document.getElementById("penny-img").src = "assets/images/penny-heads.jpg"
+        document.getElementById("heads").textContent = (timesHeads)
+        document.getElementById("responder").textContent = "You rolled heads!"
+        percHeads = (Math.round(timesHeads / totalFlips * 100))
+        percTails = (Math.round(timesTails / totalFlips * 100))
+        document.getElementById("heads-percent").textContent = (`${percHeads}%`)
+        document.getElementById("tails-percent").textContent = (`${percTails}%`)
+    }
+})
+
+document.getElementById("clear").addEventListener("click", function(){
+    totalFlips = timesHeads = timesTails = percHeads = percTails = 0
+    document.getElementById("heads-percent").textContent = (`${percHeads}%`)
+    document.getElementById("tails-percent").textContent = (`${percTails}%`)
+    document.getElementById("heads").textContent = (timesHeads)
+    document.getElementById("tails").textContent = (timesTails)
+    document.getElementById("responder").textContent = "Cleared the scoreboard!"
+})
